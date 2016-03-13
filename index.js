c0c72293bc76653e13c55abf5eee85c66e4fcd2e
James Blanksby <james@thisismyengine.com>
2016-03-13 12:53:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var data    = require('./words.js');
+var program = require('commander');
+var colors  = require('colors');
+
+program
+    .version('0.0.1')
+    .parse(process.argv);
+
+// Remove node arguments
+process.argv.splice(0, 2);
+
+process.argv.forEach(function(word) {
+    var output = word.toUpperCase() + ' => ';
+    if (is_reserved(word)) {
+        output += colors.red('reserved');
+    } else {
+        output += colors.green('not reserved');
+    }
+    console.log(output);
+});
+
+function is_reserved(word) {
+    return (data.words.indexOf(word.toUpperCase()) != -1);
+}
