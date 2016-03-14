58ac7e7141eb8809fe27041b9327f606c5392b5b
James Blanksby <james@thisismyengine.com>
2016-03-14 15:42:15 +0000

    Updated to check whether or not word is a reserved or keyword

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var data    = require('./words.js');
+var data    = require('./data.js');
 var program = require('commander');
 var colors  = require('colors');
 
@@ -12,9 +12,11 @@ program
 process.argv.splice(0, 2);
 
 process.argv.forEach(function(word) {
-    var output = word.toUpperCase() + ' => ';
+    var output = colors.white(word.toUpperCase() + ' => ');
     if (is_reserved(word)) {
         output += colors.red('reserved');
+    } else if (is_keyword(word)) {
+    	output += colors.yellow('keyword');
     } else {
         output += colors.green('not reserved');
     }
@@ -22,5 +24,9 @@ process.argv.forEach(function(word) {
 });
 
 function is_reserved(word) {
-    return (data.words.indexOf(word.toUpperCase()) != -1);
+    return (data.reserved.indexOf(word.toUpperCase()) != -1);
+}
+
+function is_keyword(word) {
+	return (data.keywords.indexOf(word.toUpperCase()) != -1);
 }
